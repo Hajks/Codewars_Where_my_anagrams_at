123af70c8d4d61e6a2bd678ff4de091bf6d7152a
Hajk <tunczorx@gmail.com>
2018-10-12 17:21:22 +0200

    Codewars task number 9. Description inside.

--- Codewars_Where_my_anagrams_at/Program.cs
+++ Codewars_Where_my_anagrams_at/Program.cs
@@ -10,6 +10,30 @@ namespace Codewars_Where_my_anagrams_at
     {
         static void Main(string[] args)
         {
+            //MySolution
+            List<string> Anagrams(string word, List<string> words)
+            {
+                List<string> listToReturn = new List<string>();
+                string sortedWord = String.Concat(word.OrderBy(c => c));
+                foreach (string item in words)
+                {
+                    if (sortedWord == String.Concat(item.OrderBy(c => c)))
+                    {
+                        listToReturn.Add(item);
+                    }
+                }
+                return listToReturn;
+            }
+
+            //Codewars solution + code review for myself
+
+            List<string> Anagrams2(string word, List<string> words)
+            {
+                var pattern = word.OrderBy(p => p).ToArray(); //We create pattern for our word 
+                return words.Where(item => item.OrderBy(p => p).SequenceEqual(pattern)).ToList(); //Now we sort words from list in the same way like we did with our pattern
+                                                                                                  //Later we compare it to our pattern and add word which got same amount of letters. 
+            }
         }
+
     }
 }
