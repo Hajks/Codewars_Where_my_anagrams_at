c8d20a144e20d3fe2db33b661ccf3f0802f3e558
Hajk <tunczorx@gmail.com>
2018-10-12 15:47:25 +0200

    Add project files.

---
+++ Codewars_Where_my_anagrams_at/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_Where_my_anagrams_at
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
